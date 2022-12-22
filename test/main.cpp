b953f58c7055dca6581137b0820a3953488f63c4
Zhe Lei <leizhe1006@gmail.com>
2022-12-22 20:40:23 +0800

    :sparkles: add multi-threads test

--- test/main.cpp
+++ test/main.cpp
@@ -5,6 +5,19 @@
 
 static int exit_sig = 0;
 
+void thread_func(std::string url)
+{
+    RTSPHandle handle = RTSPHandle(url);
+    cv::Mat frame;
+    while (!exit_sig)
+    {
+        if (handle.getFrame(frame))
+        {
+            std::cout << "Thread id:" << std::this_thread::get_id() << " " << "get frame: " << frame.size().width << " x " << frame.size().height << std::endl;
+        }
+    }
+}
+
 void exit_signal(int sig)
 {
     exit_sig = 1;
@@ -15,17 +28,21 @@ int main(int argc, char **argv)
     commons::Log::init("./");
     std::string url = std::string(argv[1]);
     SPDLOG_DEBUG("url: {}", url.c_str());
-    RTSPHandle handle = RTSPHandle(url);
 
     signal(SIGTERM, exit_signal);
 
-    cv::Mat frame;
-    while (!exit_sig)
+    const int n = 8;
+    std::array<std::thread, n> threads;
+
+    for (auto &t : threads)
     {
-        if (handle.getFrame(frame))
-        {
-            SPDLOG_INFO("get frame: {} x {} -t {}", frame.size().width, frame.size().height, Utils::getCurrentTime().c_str());
-            // std::this_thread::sleep_for(std::chrono::milliseconds(50));
-        }
+        t = std::thread(thread_func, url);
     }
+
+    for (auto &t : threads)
+    {
+        t.join();
+    }
+
+    return 0;
 }
